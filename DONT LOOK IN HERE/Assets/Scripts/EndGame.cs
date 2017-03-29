541c0fe0bf7e2492cc6160834f04fba9ab6f7bf9
james.lyle <james.lyle@SEA-8D595E>
2017-03-29 09:33:48 -0700

    Add for assessment

---
+++ DONT LOOK IN HERE/Assets/Scripts/EndGame.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndGame : MonoBehaviour
+{
+
+    void OnTriggerEnter(Collider colInfo)
+    {
+        if (colInfo.CompareTag("COllideable"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+
+}
---
+++ DONT LOOK IN HERE/Assets/Scripts/Spinner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spinner : MonoBehaviour {
+
+    // Use this for initialization
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.Rotate(Vector3.up, Time.deltaTime);     // mul*deltaTime by some speed
+        rb.AddTorque(Vector3.up, ForceMode.Acceleration); // control speed with angular drag and mass
+    }
+}
